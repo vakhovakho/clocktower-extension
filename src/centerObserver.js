4ddb27b5acaba960ccbaa73be21544026ca93cab
Vakho Nakashidze <vakho.nakashidze@gmail.com>
2024-02-02 17:37:32 +0400

    merge latest changes

--- src/centerObserver.js
+++ src/centerObserver.js
@@ -8,25 +8,26 @@ const observer = new MutationObserver(records => {
 	let stDivsOutside = document.querySelectorAll('ul.storytellers li.storyteller');
 	for (let i = 0; i < stDivsOutside.length; i++) {
 		let name = stDivsOutside[i].querySelector('div.name span')?.innerText;
-		for (let i = 0; i < stDivsInChat.length; i++) {
-			let inChatChat = stDivsInChat[i].querySelector('.chat');
-			let inChatVideo = stDivsInChat[i].querySelector('.video');
+		for (let j = 0; j < stDivsInChat.length; j++) {
+			let inChatChat = stDivsInChat[j].querySelector('.chat');
+			let inChatVideo = stDivsInChat[j].querySelector('.video');
 			let inChatAudio = stDivsInChat[i].querySelector('.audio');
-			let inChatNameDiv = stDivsInChat[i].querySelector('.name');
-			let inChatTokenDiv = stDivsInChat[i].querySelector('.token');
+			let inChatNameDiv = stDivsInChat[j].querySelector('.name');
+			let inChatTokenDiv = stDivsInChat[j].querySelector('.token');
 			let inChatName = inChatNameDiv.innerText.trim();
 			if (inChatName.startsWith(name)) {
-				stDivsInChat[i].style.position = 'fixed';
-				stDivsInChat[i].style.bottom = '106px';
-				stDivsInChat[i].style.width = 'min(26vw, 26vh)';
-				stDivsInChat[i].style[i === 0 ? 'left' : 'right'] = '80px';
+				stDivsInChat[j].style.position = 'fixed';
+				stDivsInChat[j].style.bottom = '106px';
+				stDivsInChat[j].style.width = 'min(26vw, 26vh)';
+				stDivsInChat[j].style[i === 0 ? 'left' : 'right'] = '80px';
 				inChatChat.style.borderRadius = '50%';
 				inChatVideo.style.borderRadius = '50%';
 				inChatVideo.style.borderColor = '#6fbc6f';
 
 				inChatNameDiv.style.display = 'none';
 				inChatTokenDiv.style.display = 'none';
-
+				inChatAudio.style.display = 'none';
+				break;
 			}
 		}
 	}
